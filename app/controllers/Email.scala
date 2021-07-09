80ee92637cf9aa81fb8b0be2830beb5040fe265d
Arun Yadav <GSC-30710@GSLAB.COM>
2021-07-09 13:16:56 +0530

    added some more code

--- app/controllers/Email.scala
+++ app/controllers/Email.scala
@@ -1,29 +1,60 @@
 package controllers
 
-import org.apache.commons.mail.{HtmlEmail, SimpleEmail, DefaultAuthenticator}
+import com.datastax.driver.core.Row
+import org.apache.commons.mail.{DefaultAuthenticator, HtmlEmail, SimpleEmail}
+import play.api.Configuration
 import play.api.mvc.{BaseController, ControllerComponents}
+import utils.CassandraClient
 
 import javax.inject.Inject
+import scala.jdk.CollectionConverters.IterableHasAsScala
 
+class Email @Inject() (val controllerComponents: ControllerComponents,cassandraClient: CassandraClient,config : Configuration) extends BaseController {
 
-
-class Email @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
+  val session = cassandraClient.session
+  private val emailId= config.get[String]("email")
+  private val password = config.get[String]("password")
+  val templateType= "subscribe"
+  val keyspace = "arun"
+  val emailList = "emaillist"
+  val template = "templates"
+  val getEmailQ = session.prepare(s"select * from $keyspace.$emailList")
+  val getTemplateQ = session.prepare(s"select content, subject from $keyspace.$template where type = ? ")
 
   def send= Action{ implicit request =>
 
     val email = new SimpleEmail
-    email.setAuthentication("arun952138@gmail.com","Arun1234")
-    email.addTo("fatearun428@gmail.com")
-    email.setFrom("arun952138@gmail.com")
-    email.setSubject("demo mail")
+
+    //val emails= Seq("fatearun428@gmail.com","aruny428@gmail.com")
+    val emails = getEmail()
+    email.setAuthentication(emailId,password)
+    emails.foreach(em => email.addTo(em))
+    email.setFrom(emailId,"Arun")
+    email.setSubject(getTemplate(templateType).get.Subject)
     email.setHostName("smtp.gmail.com")
-    email.setMsg("this is a test mail....")
+    email.setMsg(getTemplate(templateType).get.body)
     email.setSmtpPort(465)
-    email.setStartTLSEnabled(true)
+    email.setSSL(true)
+    //email.setStartTLSEnabled(true)
     email.send
     Ok("email sent")
   }
 
+  def getEmail()={
+    session.execute(getEmailQ.bind()).asScala.map(r => r.getString("email")).toSeq
+  }
 
+  def getTemplate(templateType : String ) = {
+    session.execute(getTemplateQ.bind(templateType)).asScala.map(toTemplate).headOption
+  }
+
+  def toTemplate(r :Row)={
+    Template(r.getString("content"),r.getString("subject"))
+  }
+
+  case class Template(
+                     body : String,
+                     Subject : String
+                     )
 
 }
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -13,6 +13,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
     Ok("4th attempt !!")
   }
 
+  // not in use right now (not working)
   def send()= Action {implicit request =>
 
     val props = System.getProperties
@@ -20,8 +21,9 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
     props.put("mail.transport.protocol", "smtp")
     props.put("mail.smtp.port", "465")
     props.put("mail.smtp.auth", "true")
-    props.put("mail.smtp.starttls.enable", true)
-    props.put("mail.smtp.starttls.required", true)
+    //props.put("email.smtp.ssl",true)
+    //props.put("mail.smtp.starttls.enable", true)
+    //props.put("mail.smtp.starttls.required", true)
 
     val session = Session.getDefaultInstance(props)
 
@@ -36,7 +38,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
     val transport = session.getTransport
     println("sending mail begins ")
 
-    transport.connect("smtp.gmail.com", 465, "arun952138@gmail.com", "Arun1234")
+    transport.connect("smtp.gmail.com", 465, "arun952138@gmail.com", "Arun1234@")
     transport.sendMessage(msg, msg.getAllRecipients)
 
     Ok("mail sent !!")
